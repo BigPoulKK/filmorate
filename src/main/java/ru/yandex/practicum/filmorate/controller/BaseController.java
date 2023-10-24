335adbad061c66e826f8d8e932a7705730e9c9a7
BigPoulK <BigPoulK@yandex.ru>
2023-10-24 18:12:38 +0700

    cl

--- src/main/java/ru/yandex/practicum/filmorate/controller/BaseController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/BaseController.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.model.BaseUnit;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,21 +13,21 @@ public abstract class BaseController<T extends BaseUnit> {
     private final Map<Long, T> storage = new HashMap<>();
     private long generatedId;
 
-    public T creat(T data){
+    public T creat(T data) {
         data.setId(++generatedId);
         storage.put(data.getId(), data);
         return data;
     }
 
     public T update(T data) {
-        if(!storage.containsKey(data.getId())){
+        if (!storage.containsKey(data.getId())){
             throw new DataNotFoundException(String.format("Data %s not found", data));
         }
         storage.put(data.getId(), data);
         return data;
     }
 
-    public List<T> getAll(){
+    public List<T> getAll() {
         return new ArrayList<>(storage.values());
     }
 }
